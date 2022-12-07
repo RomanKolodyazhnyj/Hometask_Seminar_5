a17ba7b0254363cc6b5975e0deabd2bacdb6273c
Roman <romek20@mail.ru>
2022-12-07 16:35:23 +0300

    Решение задачи 38

---
+++ Task_38/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+var array = FillArray(5);
+Print(array);
+Console.WriteLine();
+
+int max = Max(array);
+int min = Min(array);
+Console.WriteLine($"Максимальное значение: {max}, минимальное значение: {min}");
+
+int result = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {result}");
+
+int Max(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (max < array[i]) 
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int Min(int[] array)
+{
+    int min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (min > array[i]) 
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(10, 100);
+    }
+    return array;
+}
+
+void Print(int[] array)
+{
+    foreach(var item in array)
+    {
+        Console.Write($"{item}, ");
+    }
+}
